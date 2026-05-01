4aa222d94a2ef85ab4615e5b9b4b7afcb1430115
vorgxd <vorgxd@gmail.com>
2026-05-01 17:31:20 +0530

    Include multiple projects in settings.gradle.kts
    asdfghjkl

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,8 @@ File(rootDir, ".").eachDir { dir ->
 fun File.eachDir(block: (File) -> Unit) {
     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
-
-// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-// include("PluginName")
+include(":AnimeKai")
+include(":AnimePahe")
+include(":Miruro")
+include(":Comix")
+include(":MangaFire")
